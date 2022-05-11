a8a9de1c5b3b2948240bdaf89c761628981ece82
Nithin S Varrier <nithin486@hotmail.com>
2022-05-11 23:12:10 +0530

    Added initial repo

--- src/components/App/index.tsx
+++ src/components/App/index.tsx
@@ -1,24 +1,31 @@
-import React from "react";
-import logo from "../../assets/logo.svg";
-import "./App.scss";
+import logo from "../../assets/logo.png";
+import classes from "./App.module.scss";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className={classes.container}>
+      <header className={classes.header}>
+        <div className="title">IEEE Richard E Merwin Scholarship</div>
+        <div className="search"></div>
       </header>
+      <main>
+        <div className={classes.coverImg}>
+          <img
+            src={logo}
+            aria-hidden="true"
+            className={classes.logo}
+            alt="Logo"
+          />
+        </div>
+        <section className={classes.wrapper}>
+          <section className={classes.content}>
+            <div className={classes.contentWrapper}>
+              <h1>IEEE Richard E Merwin Scholarship </h1>
+            </div>
+          </section>
+          <section className={classes.rightSidebar}></section>
+        </section>
+      </main>
     </div>
   );
 }
