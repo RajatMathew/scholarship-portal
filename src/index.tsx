48875bef19491d085ea4c1c4b479bb81de6acfeb
Nithin S Varrier <nithin486@hotmail.com>
2022-05-22 21:13:07 +0530

    Add homepage layout

--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,6 @@
 import React from "react";
+import "react-app-polyfill/ie11";
+import "react-app-polyfill/stable";
 import ReactDOM from "react-dom/client";
 import App from "./feature/App";
 import "./styles/styles.scss";
