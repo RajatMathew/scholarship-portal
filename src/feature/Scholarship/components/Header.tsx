2f4b10792b5ac0fa67a722a2b6cd76d7519b9a1b
Nithin S Varrier <nithin486@hotmail.com>
2022-07-01 21:49:39 +0530

    Change PNGs to WEBP

--- src/feature/Scholarship/components/Header.tsx
+++ src/feature/Scholarship/components/Header.tsx
@@ -1,5 +1,5 @@
 import { Link } from "react-router-dom";
-import Logo from "../../../assets/main-logo-Banner.png";
+import Logo from "../../../assets/main-logo-Banner.webp";
 import classes from "./Header.module.scss";
 function Header() {
   return (
